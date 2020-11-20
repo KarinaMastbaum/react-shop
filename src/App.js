a568fc362ed56534eda2d355484c4305f5d3e6a7
Karina <karina.mastbaum@gmail.com>
2020-11-19 22:04:43 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Carrousel from './components/Carrousel';
+import carrousel from './components/Carrousel';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header>
+        <a href="/">Kari Shop</a>
       </header>
+      <main className="fondo">
+        {/* <Carrousel/> */}
+        Productos
+      </main>
+      <footer>
+        Todos los derechos reservados.
+      </footer>
     </div>
   );
 }
